2fca885f774d0f2f8bb750d0dc4e5bed3eebfc41
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-12-03 03:00:44 +0000

    Test part

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,31 @@
 const request = require('request');
 
 
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const breedName = process.argv[2];
 
-request(url, (error, responce, body) => {
-  let data = JSON.parse(body);
-  // console.log(typeof(data));
-  if (error) {
-    console.log("This URL does not exist");
-  } else if (data.length < 1) {
-    console.log("Breed wasn't found!");
-  } else if (breed) {
-    console.log(data[0].description);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, responce, body) => { 
+        
+
+    if (error) {
+      callback(error, null);
+      return;      
+    }    
+    
+    let data = JSON.parse(body);
+    
+    if (error || data.length < 1) {
+      callback("Breed wasn't found!");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
 
 
  
